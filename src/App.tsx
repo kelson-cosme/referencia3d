71e55fe1d784a1918914b177647b442a73125113
kelson-cosme <kelson.almeida123@gmail.com>
2025-12-04 15:52:30 -0400

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
 import './App.css'
+import PixelTrail from '@/components/PixelTrail';
 
 function App() {
 
   return (
     <>
-    <h1 className='text-red-500'>teste</h1>
+     <PixelTrail
+    gridSize={50}
+    trailSize={0.1}
+    maxAge={250}
+    interpolate={5}
+    color="#fff"
+    gooeyFilter={{ id: "custom-goo-filter", strength: 2 }}
+  />
     </>
   )
 }
